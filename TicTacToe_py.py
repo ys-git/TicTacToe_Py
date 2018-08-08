4fea79c5a1299b202ad20dd879760158a0c6e7df
YS <yogendrasingh890@gmail.com>
2018-08-08 19:39:17 +0530

    major improvements.

--- TicTacToe_py.py
+++ TicTacToe_py.py
@@ -3,7 +3,7 @@
 import random
 
 
-ls=[0, 1, 2, 3, 4, 5, 6, 7, 8]
+ls=[1, 2, 3, 4, 5, 6, 7, 8, 9]
 res=0
 a='A'
 b='A'
@@ -32,13 +32,79 @@ def play():
     t = 'X'
     while chk() == True:
         holder = input(f"Enter the position to place {t}")
-        holder=int(holder)
+        holder=int(holder-1)
         ls[holder] = t
         if t == 'X':
             t = 'O'
         elif t == 'O':
             t = 'X'
         printboard()
+
+def winchk():
+    win='None'
+
+    if ls[0]==ls[1]==ls[2]=='X':
+        win='X'
+    elif ls[0]==ls[1]==ls[2]=='O':
+        win = 'O'
+
+    if ls[3]==ls[4]==ls[5]=='X':
+        win='X'
+    elif ls[3]==ls[4]==ls[5]=='O':
+        win = 'O'
+
+    if ls[6]==ls[7]==ls[8]=='X':
+        win='X'
+    elif ls[6]==ls[7]==ls[8]=='O':
+        win = 'O'
+
+    if ls[0]==ls[3]==ls[6]=='X':
+        win='X'
+    elif ls[0]==ls[3]==ls[6]=='O':
+        win = 'O'
+
+    if ls[1]==ls[4]==ls[7]=='X':
+        win='X'
+    elif ls[1]==ls[4]==ls[7]=='O':
+        win = 'O'
+
+    if ls[2]==ls[5]==ls[8]=='X':
+        win='X'
+    elif ls[0]==ls[1]==ls[2]=='O':
+        win = 'O'
+
+    if ls[0]==ls[4]==ls[6]=='X':
+        win='X'
+    elif ls[0]==ls[4]==ls[6]=='O':
+        win = 'O'
+
+    if ls[2]==ls[4]==ls[8]=='X':
+        win='X'
+    elif ls[2]==ls[4]==ls[8]=='O':
+        win = 'O'
+
+    if ls[0]==ls[4]==ls[2]=='X':
+        win='X'
+    elif ls[0]==ls[4]==ls[2]=='O':
+        win = 'O'
+
+    if ls[6]==ls[4]==ls[8]=='X':
+        win='X'
+    elif ls[6]==ls[4]==ls[8]=='O':
+        win = 'O'
+
+    if ls[0]==ls[4]==ls[8]=='X':
+        win='X'
+    elif ls[0]==ls[4]==ls[8]=='O':
+        win = 'O'
+
+    if ls[6]==ls[4]==ls[2]=='X':
+        win='X'
+    elif ls[6]==ls[4]==ls[2]=='O':
+        win = 'O'
+
+
+
 
 x = random.randint(0, 5)
 if x >= 0 and x <= 2:
