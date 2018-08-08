535d17137579698d4874438d80076836acdf1a44
YS <yogendrasingh890@gmail.com>
2018-08-08 16:53:44 +0530

    Initial push.

--- TicTacToe_py.py
+++ TicTacToe_py.py
@@ -1,3 +1,11 @@
-#Tic Tac Toe game in python
 
+ls=[0, 1, 2, 3, 4, 5, 6, 7, 8]
 res=False
+p1=input("Enter the name of Player 1:")
+p2=input("Enter the name of Player 1:")
+
+
+def printboard():
+    print(f"{ls[0]}  |  {ls[1]} |  {ls[2]} \n{ls[3]}  |  {ls[4]} |  {ls[5]}\n{ls[6]}  |  {ls[7]} |  {ls[8yield ]} ")
+
+printboard()
