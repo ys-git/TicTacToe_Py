4a8580c960091258275c9d36025a3e2b4d1c580f
YS <yogendrasingh890@gmail.com>
2018-08-08 17:20:46 +0530

    added some more logic

--- TicTacToe_py.py
+++ TicTacToe_py.py
@@ -2,7 +2,8 @@ import random
 
 
 ls=[0, 1, 2, 3, 4, 5, 6, 7, 8]
-res=False
+res=0
+again=True
 p1=input("Enter the name of Player 1:")
 p2=input("Enter the name of Player 1:")
 
@@ -12,10 +13,25 @@ def printboard():
           f"{ls[3]}  |  {ls[4]} |  {ls[5]} \n------------\n"
           f"{ls[6]}  |  {ls[7]} |  {ls[8]} ")
 
-x=random.randint(0,5)
-if x>=0 and x<=2:
-    print("Player 1 will play the first move\nPlayer 1 : X\nPlayer 2 : O")
-else:
-    print("Player 2 will play the first move\nPlayer 2 : O\nPlayer 1 : X")
+while again:
+    x = random.randint(0, 5)
+    if x >= 0 and x <= 2:
+        print("Player 1 will play the first move\nPlayer 1 : X\nPlayer 2 : O")
+    else:
+        print("Player 2 will play the first move\nPlayer 2 : O\nPlayer 1 : X")
 
-holder=input(f"Enter the position to place {}")
+        printboard()
+
+        res=play()
+
+
+
+def play():
+    w
+    holder = input(f"Enter the position to place {}")
+
+
+
+
+
+
